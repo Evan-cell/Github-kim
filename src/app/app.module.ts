49deeb16bb97fc166bccffe2a86b0ac021a6f0d2
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-08-28 14:36:56 +0300

    created github-profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubAppComponent } from './components/github-app/github-app.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubAppComponent
+    GithubAppComponent,
+    GithubProfileComponent
   ],
   imports: [
     BrowserModule,
